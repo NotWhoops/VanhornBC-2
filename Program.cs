c4648c57c1fd89612ec8d968eb08a089b84b9d2d
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-05 13:22:18 -0800

    not sure

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using VanhornBC2.Services;
+using VanhornBMC5.Services;
+using VanhornBMC6.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AddingTwoNumbersServices>();
+builder.Services.AddScoped<AskingQuestionsServices>();
+builder.Services.AddScoped<GuessItServices>();
+builder.Services.AddScoped<HelloWorldServices>();
+builder.Services.AddScoped<MadLibServices>();
+builder.Services.AddScoped<Magic8BallServices>();
+builder.Services.AddScoped<OddOrEvenServices>();
+builder.Services.AddScoped<RestaurantPickerServices>();
+builder.Services.AddScoped<ReverseItAlphanumericServices>();
+builder.Services.AddScoped<ReverseItNumbersServices>();
 
 var app = builder.Build();
 
