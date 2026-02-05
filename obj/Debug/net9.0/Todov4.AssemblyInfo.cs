d93f290ff9d681bf9f361cb167daf720806c748d
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-05 13:21:07 -0800

    scoped services to the program.cs

--- obj/Debug/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todov4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba5e888dcff6ee693312c91010086df4aa9c76d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420ca5dc2194a63161a7990b4fb29e8bcb58376f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
