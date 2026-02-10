4afa6cfd921b251b1ab1a3b56d4f0c03ba479e06
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-10 09:30:23 -0800

    read me stuff

--- obj/Debug/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todov4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff00b102713edd9fd03f47655493b5faada0c123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9017ee7769e1c16e34e335945745db0881a70dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
