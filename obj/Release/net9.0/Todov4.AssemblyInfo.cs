26ad16f8fce9491644b511a3034734cefce67caa
Brandon Van Horn <bmdvanhorn@icloud.com>
2026-02-10 10:14:13 -0800

    stuff

--- obj/Release/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Release/net9.0/Todov4.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afa6cfd921b251b1ab1a3b56d4f0c03ba479e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
